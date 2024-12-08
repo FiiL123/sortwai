92aae03d094e72d3bda4e98ae81756a0c4e54377
Adam Zahradník <adam@zahradnik.xyz>
2024-12-08 21:25:19 +0100

    add category list

--- sortwai/urls.py
+++ sortwai/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sortwai.waste.views import CategoryListView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", CategoryListView.as_view(), name="category_list"),
 ]
--- sortwai/waste/views.py
+++ sortwai/waste/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.views.generic import ListView
+from sortwai.waste.models import Category, Municipality
+
+
+def get_active_municipality(request):
+    if not hasattr(request, "_municipality"):
+        request._municipality = Municipality.objects.first()
+
+    return request._municipality
+
+
+class CategoryListView(ListView):
+    template_name = "categories.html"
+
+    def get_queryset(self):
+        return Category.objects.filter(municipality=get_active_municipality(self.request))
