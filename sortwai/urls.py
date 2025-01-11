fbf1380b384cbba5b81e5c914b43d7700c62bad0
Filipko^^ <fiilgameslp@gmail.com>
2025-01-11 14:47:20 +0100

    add scanner ui

--- sortwai/urls.py
+++ sortwai/urls.py
@@ -14,18 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from sortwai.waste.views import CategoryListView, DocumentListView, LocationListView
+from sortwai.waste.views import (
+    CategoryListView,
+    DocumentListView,
+    LocationListView,
+    ScannerView,
+    get_trash,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", CategoryListView.as_view(), name="category_list"),
     path("docs/", DocumentListView.as_view(), name="document_list"),
     path("locations/", LocationListView.as_view(), name="location_list"),
+    path("scanner/", ScannerView.as_view(), name="scanner"),
+    path("scanner/<str:code>", get_trash, name="scanner_product"),
 ]
 
 if settings.DEBUG:
--- sortwai/waste/views.py
+++ sortwai/waste/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView
-from sortwai.waste.models import Category, Municipality, Document, Location
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+
+from sortwai.waste.models import BarCode, Category, Document, Location, Municipality
 
 
 def get_active_municipality(request):
@@ -13,23 +16,43 @@ class CategoryListView(ListView):
     template_name = "categories.html"
 
     def get_queryset(self):
-        return Category.objects.filter(municipality=get_active_municipality(self.request)).select_related("target")
+        return Category.objects.filter(
+            municipality=get_active_municipality(self.request)
+        ).select_related("target")
 
 
 class DocumentListView(ListView):
     template_name = "docs.html"
 
     def get_queryset(self):
-        return Document.objects.filter(municipality=get_active_municipality(self.request))
+        return Document.objects.filter(
+            municipality=get_active_municipality(self.request)
+        )
 
 
 class LocationListView(ListView):
     template_name = "locations.html"
 
     def get_queryset(self):
-        return Location.objects.filter(municipality=get_active_municipality(self.request), show_on_main=True).prefetch_related("targets")
+        return Location.objects.filter(
+            municipality=get_active_municipality(self.request), show_on_main=True
+        ).prefetch_related("targets")
 
-    def get_context_data(self, *, object_list = ..., **kwargs):
+    def get_context_data(self, *, object_list=..., **kwargs):
         ctx = super(LocationListView, self).get_context_data(**kwargs)
         ctx["map_pins"] = list(self.get_queryset().values_list("gps_lat", "gps_lon"))
         return ctx
+
+
+class ScannerView(TemplateView):
+    template_name = "scanner.html"
+
+
+def get_trash(request, code):
+    # item = get_object_or_404(BarCode, code=code)
+    item = BarCode.objects.filter(code=code)
+    if not item:
+        return HttpResponse('<div id="qr-reader-results"><p>NOT FOUND</p></>')
+    item = item.first()
+    context = {"item": item}
+    return render(request, "partials/product.html", context)
