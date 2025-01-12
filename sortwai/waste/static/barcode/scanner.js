0430ddf667befdecd028a03bf0a764fb59a20a70
Filipko^^ <fiilgameslp@gmail.com>
2025-01-12 08:22:16 +0100

    move scanner js to file

---
+++ sortwai/waste/static/barcode/scanner.js
@@ -0,0 +1,25 @@
+var resultContainer = document.getElementById('qr-reader-results');
+var lastResult, countResults = 0;
+
+async function onScanSuccess(decodedText, decodedResult) {
+    if (decodedText !== lastResult) {
+        ++countResults;
+        lastResult = decodedText;
+        let url = "/scanner/" + decodedText;
+
+        htmx.ajax('GET', url, {
+            target: '#qr-reader-results', // Replace with the ID of your target container
+            swap: 'outerHTML'          // Optional: specify the swap strategy
+        });
+
+        console.log(`Scan result ${decodedText}`, decodedResult);
+    }
+}
+var scanner = document.getElementById('qr-reader');
+var screen_w = scanner.offsetWidth/3;
+var screen_h = scanner.offsetWidth/6;
+console.log(screen_h)
+console.log(screen_w)
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-reader", { fps: 10, qrbox: {width: screen_w, height: screen_h}});
+html5QrcodeScanner.render(onScanSuccess);
