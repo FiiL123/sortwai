92d81ab8ff23e21661d23aa3dbd33d0c1b834759
Filipko^^ <fiilgameslp@gmail.com>
2025-01-12 08:33:29 +0100

    use cookie to store location

--- sortwai/waste/static/js/location.js
+++ sortwai/waste/static/js/location.js
@@ -1,16 +1,35 @@
 navigator.geolocation.getCurrentPosition(getCity);
 async function getCity(position) {
+    cookieVal = getCookie("municipality")
+    if ( cookieVal != null){
+        document.getElementById('municipality').innerHTML = cookieVal;
+        return;
+    }
     fetch('/get_location/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ latitude: position.coords.latitude, longitude: position.coords.longitude }), // Example coordinates for Bratislava
-    })
-.then(response => response.json())
-        .then(data => {
-            console.log('City:', data.city);
-            document.getElementById('municipality').innerHTML = data.city;
-        })
-        .catch(error => console.error('Error:', error));
+        body: JSON.stringify({ latitude: position.coords.latitude, longitude: position.coords.longitude }),
+    }).then(response => response.json())
+            .then(data => {
+                console.log('City:', data.city);
+                document.getElementById('municipality').innerHTML = data.city;
+                document.cookie=`municipality=${data.city}; max_age=`+(24*60*60);
+            })
+            .catch(error => console.error('Error:', error));
+    }
+
+
+function getCookie(name) {
+    let cookieArr = document.cookie.split(";");
+
+    for(let i = 0; i < cookieArr.length; i++) {
+        let cookiePair = cookieArr[i].split("=");
+
+        if(name == cookiePair[0].trim()) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    return null;
 }
