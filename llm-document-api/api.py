f5b6a4c4a146385011072fbba30754ec2dbab448
David Krchňavý <david.krchnavy@gmail.com>
2025-01-13 17:11:51 +0100

    api.py: add request model

--- llm-document-api/api.py
+++ llm-document-api/api.py
@@ -3,6 +3,12 @@ from langchain_core.documents import Document
 from langchain_experimental.graph_transformers import LLMGraphTransformer
 from langchain_neo4j import Neo4jGraph
 from langchain_openai import AzureChatOpenAI
+from pydantic import BaseModel
+
+
+class DocumentRequest(BaseModel):
+    document: str
+
 
 llm = AzureChatOpenAI(
     deployment_name="ace-gpt-4o-mini",
@@ -29,13 +35,12 @@ llm_transformer = LLMGraphTransformer(
     """,
 )
 graph = Neo4jGraph(refresh_schema=False)
-
 app = FastAPI()
 
 
 @app.post("/llm-document")
-def add_to_graph(document: str):
-    chunks = [Document(chunk.strip()) for chunk in document.split("---")]
+def add_to_graph(request: DocumentRequest):
+    chunks = [Document(chunk.strip()) for chunk in request.document.split("---")]
     graph_documents = llm_transformer.convert_to_graph_documents(chunks)
     graph.add_graph_documents(graph_documents, include_source=True)
-    return graph_documents
+    return {"graph_documents": graph_documents}
