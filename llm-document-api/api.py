3ee07306f5030f3d6df88ec91f0b315bb5a843e6
David Krchňavý <david.krchnavy@gmail.com>
2025-01-13 20:26:28 +0100

    api.py: add document metadata

--- llm-document-api/api.py
+++ llm-document-api/api.py
@@ -8,8 +8,15 @@ from langchain_openai import AzureChatOpenAI
 from pydantic import BaseModel
 
 
+class DocumentModel(BaseModel):
+    name: str
+    city: str
+    content: str
+
+
 class DocumentRequest(BaseModel):
-    document: str
+    document: DocumentModel
+    split_into_chunks: bool = True
 
 
 llm = AzureChatOpenAI(
@@ -42,7 +49,21 @@ app = FastAPI()
 
 @app.post("/llm-document")
 def add_to_graph(request: DocumentRequest):
-    chunks = [Document(chunk.strip()) for chunk in request.document.split("---")]
+    document = request.document
+    if request.split_into_chunks:
+        chunks = [
+            Document(
+                chunk.strip(),
+                metadata={
+                    "name": document.name,
+                    "city": document.city,
+                    "chunk_number": n,
+                },
+            )
+            for n, chunk in enumerate(document.content.split("---"))
+        ]
+    else:
+        chunks = [Document(document.content.strip(), metadata={"name": document.name})]
     graph_documents = llm_transformer.convert_to_graph_documents(chunks)
     graph.add_graph_documents(graph_documents, include_source=True)
     return {"graph_documents": graph_documents}
