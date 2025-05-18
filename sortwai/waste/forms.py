3acdb675377a504de14d4b3d4598b5a4dcddf5f8
Filip Sršeň <fiilgameslp@gmail.com>
2025-05-18 11:59:16 +0200

    fix: get_municipality call on startup

--- sortwai/waste/forms.py
+++ sortwai/waste/forms.py
@@ -4,4 +4,4 @@ from sortwai.waste.models import Municipality
 
 
 class MunicipalityForm(forms.Form):
-    municipality = forms.ChoiceField(choices=Municipality.get_municipality_choices())
+    municipality = forms.ChoiceField(choices=Municipality.get_municipality_choices)
